443bf6172c1f10495a216baa3162835018a248db
jperafan <jperafan@bancolombia.com.co>
2025-03-07 08:34:35 -0500

    Proyecto inicial

---
+++ src/test/java/principal/ExampleTests.java
@@ -0,0 +1,22 @@
+package principal;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class ExampleTests {
+    @BeforeMethod
+    public void setUp() { //precondicion
+
+    }
+
+    @Test
+    public void testPrimero() {
+
+    }
+
+    @AfterMethod
+    public void tearDown() { //postcondicion
+
+    }
+}
